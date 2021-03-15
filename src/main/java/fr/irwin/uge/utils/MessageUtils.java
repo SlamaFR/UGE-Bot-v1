691101890f0fded74dde5c925118b41ed5dc0260
notKamui <teillard.jimmy@outlook.fr>
2021-03-15 15:00:58 +0100

    Improved readability and modularity in MessageUtils

--- src/main/java/fr/irwin/uge/utils/MessageUtils.java
+++ src/main/java/fr/irwin/uge/utils/MessageUtils.java
@@ -5,20 +5,46 @@ import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
 
-public class MessageUtils
-{
-    public static void sendErrorMessage(TextChannel textChannel, String error)
-    {
-        textChannel.sendMessage(new EmbedBuilder().setTitle("Erreur").setDescription(error).setColor(new Color(0xe74c3c)).build()).queue();
+public class MessageUtils {
+    public static void sendMessage(
+        String message,
+        Color color,
+        TextChannel textChannel,
+        String error
+    ) {
+        textChannel.sendMessage(
+            new EmbedBuilder()
+                .setTitle(message)
+                .setDescription(error)
+                .setColor(color)
+                .build()
+        ).queue();
     }
 
-    public static void sendWarningMessage(TextChannel textChannel, String error)
-    {
-        textChannel.sendMessage(new EmbedBuilder().setTitle("Attention").setDescription(error).setColor(new Color(0xf1c40f)).build()).queue();
+    public static void sendErrorMessage(TextChannel textChannel, String error) {
+        sendMessage(
+            "Erreur",
+            new Color(0xe74c3c),
+            textChannel,
+            error
+        );
     }
 
-    public static void sendSuccessMessage(TextChannel textChannel, String error)
-    {
-        textChannel.sendMessage(new EmbedBuilder().setTitle("Succès").setDescription(error).setColor(new Color(0x2ecc71)).build()).queue();
+    public static void sendWarningMessage(TextChannel textChannel, String error) {
+        sendMessage(
+            "Attention",
+            new Color(0xf1c40f),
+            textChannel,
+            error
+        );
+    }
+
+    public static void sendSuccessMessage(TextChannel textChannel, String error) {
+        sendMessage(
+            "Succès",
+            new Color(0x2ecc71),
+            textChannel,
+            error
+        );
     }
 }
